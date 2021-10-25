5c4dda6e555becb3af3e76f2900d93a1d9b3bb83
Chelsea Rolland <chelsea.e.rolland@gmail.com>
2021-10-25 09:09:34 -0500

    completed the props and propstype sections

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Header from "./components/Header";
 
 function App() {
-    const name = 'Chelsea'
-    const x = true
+
   return (
     <div className="container">
         <Header />
-      {/*<h1>Hello from React</h1>*/}
-      {/*<h2>Hello {name}</h2>*/}
-      {/*<h3>Hello {x ? 'Yes' : 'No'}</h3>*/}
+
     </div>
   );
 }
