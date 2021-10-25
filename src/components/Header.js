077f736c22d4f1da4155a39a4a4a4ef079909e3b
Chelsea Rolland <chelsea.e.rolland@gmail.com>
2021-10-25 12:50:00 -0500

    about halfway through with the course - icons with react icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types'
-
+import Button from './Button'
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log("Click")
+    }
+
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color= 'green' text='Add' onClick={onClick}/>
         </header>
     )
 }
@@ -15,6 +20,12 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black'
 }
 
 export default Header
