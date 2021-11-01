5ba7aa34a804e9390631ac22049ca2065aa8a8c1
Chelsea Rolland <chelsea.e.rolland@gmail.com>
2021-11-01 10:47:10 -0500

    reloaded my backend json server, added routing 1st phase, and created footer and about components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -78,6 +79,7 @@ const toggleReminder = async (id) => {
         <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
         {showAddTask && <AddTask onAdd={addTask} />}
         {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : ('No Tasks to Show')}
+        <Footer />
     </div>
   );
 }
